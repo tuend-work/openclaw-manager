5452a368c65d0d6f2575cdc53dd866c98a986a66
tuend-work <121396415+tuend-work@users.noreply.github.com>
2026-03-29 04:42:30 +0700

    Update manage_ai.sh

--- manage_ai.sh
+++ manage_ai.sh
@@ -311,10 +311,11 @@ add_agent_to_group() {
             read group_id
             [ -z "$group_id" ] && return
 
-            # 1. Cấu hình Tài khoản cụ thể (Policy & AllowList)
+            # 1. Cấu hình Tài khoản cụ thể (Dựa trên Doctor warnings: Chat ID vào .groups)
             jq --arg acc "$sel_acc" --arg gid "$group_id" \
                '.channels.telegram.accounts[$acc].groupPolicy = "allowlist" | 
-                .channels.telegram.accounts[$acc].groupAllowFrom |= (. // [] | if contains([$gid]) then . else . + [$gid] end)' \
+                .channels.telegram.accounts[$acc].groups |= (. // [] | if contains([$gid]) then . else . + [$gid] end) |
+                .channels.telegram.accounts[$acc].groupAllowFrom |= (. // [])' \
                "$JSON_FILE" > "${JSON_FILE}.tmp" && mv "${JSON_FILE}.tmp" "$JSON_FILE"
 
             # 2. Tạo Binding cho Group gắn với tài khoản này
